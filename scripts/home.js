e62ea2829656dd37512f385ac2dfda928c1e8219
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-10-12 16:13:38 +0100

    passed data to footer

--- scripts/home.js
+++ scripts/home.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     const versionJson = await fetch("../media/data/version.json")
         .then(resp => resp.json())
     
-    console.log(versionJson);
+    document.getElementById("footer_link").innerText = "Developed by " + versionJson.author;
+    document.getElementById("footer_link").href = versionJson.github_url;
+    document.getElementById("footer_version").innerHTML = "V" + versionJson.version;
 });
