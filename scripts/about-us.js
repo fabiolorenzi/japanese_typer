84000760059b6cfb23a2c7ad106b62f7ea522bc3
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-10-13 10:44:11 +0100

    finished developing about us page and styled it

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -4,5 +4,8 @@ document.addEventListener("DOMContentLoaded", async function() {
     
     document.getElementById("footer_link").innerText = "Developed by " + versionJson.author;
     document.getElementById("footer_link").href = versionJson.github_url;
-    document.getElementById("footer_version").innerHTML = "V" + versionJson.version;
+    document.getElementById("footer_version").innerText = "V" + versionJson.version;
+    document.getElementById("about_version").innerText = "V" + versionJson.version;
+    document.getElementById("about_author").innerText = versionJson.author;
+    document.getElementById("about_url").href = versionJson.github_url;
 });
